9a4f1380086adab1a4c6f61ada92314d900f274e
nomii6577 <112746747+nomii6577@users.noreply.github.com>
2023-09-19 09:26:04 +0000

    datatypes

--- 01-basic/02-datatypes.js
+++ 01-basic/02-datatypes.js
@@ -5,3 +5,22 @@ console.log(3 +
     3) // code readability should be high
 
 console.log("Nouman")
+
+
+let name = "Nouman"
+let age = 17
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // datatype object
+console.log(typeof undefined); // datatype undefined
