f32cb1d85cd628f0d723df2c88cba4faa25179c0
nomii6577 <112746747+nomii6577@users.noreply.github.com>
2023-09-21 07:09:21 +0000

    datesinJS

---
+++ 01-basic/06-num-and-math.js
@@ -0,0 +1,27 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8945
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 10000000000000
+//console.log(hundreds.toLocaleString());
+
+//++++++++++++++++++++++++++++++++  Maths   +++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.6));
+// console.log(Math.floor(5.6));
+
+console.log((Math.random()*10) + 1);
+
+const min = 20
+const max = 30
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
